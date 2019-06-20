a0bad369b02632e056c4b2d1157937e99868822d
James Knighton <iamknighton@gmail.com>
2019-06-20 05:21:45 -0700

    Bump eps.

--- dreamflux/layer/instance_norm.py
+++ dreamflux/layer/instance_norm.py
@@ -1,13 +1,13 @@
 from torch import nn
 
 
-def instance_norm(x, eps=1e-6):
+def instance_norm(x, eps=1e-3):
     x = x - x.mean()
     return x / (x.std() + eps)
 
 
 class InstanceNorm(nn.Module):
-    def __init__(self, eps=1e-6):
+    def __init__(self, eps=1e-3):
         super().__init__()
         self.eps = eps
 
