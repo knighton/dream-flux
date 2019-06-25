c1716577df2ab05f03d9fec9d7253f4cfc58fc3a
James Knighton <iamknighton@gmail.com>
2019-06-24 18:12:29 -0700

    Fix.

--- dreamflux/dataset/mnist.py
+++ dreamflux/dataset/mnist.py
@@ -6,6 +6,6 @@ from torchvision import transforms as tf
 def load_mnist():
     train_dataset = MNIST(root='data/', train=True, download=True,
                           transform=tf.ToTensor())
-    val_dataset = MNIST(root='data/', train=True, download=True,
+    val_dataset = MNIST(root='data/', train=False, download=True,
                         transform=tf.ToTensor())
     return train_dataset, val_dataset
